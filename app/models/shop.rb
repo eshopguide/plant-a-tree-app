3abb146e614d97e9bec68dbb048d3d2e69255450
MariaEshopGuide <maria@eshop-guide.de>
2021-11-11 12:58:07 +0100

    Fix shop model

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -4,13 +4,13 @@ class Shop < ApplicationRecord
   include ShopifyApp::ShopSessionStorageWithScopes
   has_one :shop_settings
 
-  after_create :make_shop_setting
+  after_create :make_shop_settings
 
   def api_version
     ShopifyApp.configuration.api_version
   end
 
-
+  private
 
   def make_shop_settings
     create_shop_settings
